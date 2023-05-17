26b4ad1128584bb119042115c913f2525e646d75
LucasAlfare <god-lucas@hotmail.com>
2023-05-17 13:50:31 -0300

    cleaning up

--- src/main/kotlin/com/lucasalfare/flarcheology/ArcheologyInfoProvider.kt
+++ src/main/kotlin/com/lucasalfare/flarcheology/ArcheologyInfoProvider.kt
@@ -29,14 +29,59 @@ class ArcheologyInfoProvider(
     }
   }
 
-  // TODO
-  fun getAllSites(
-    targetLevel: Int = 120,
-    shouldContainsMaterialName: String = "",
-    shouldContainsArtefactName: String = ""
-  ): List<Site> {
-    val results = mutableListOf<Site>()
+  fun getAllSites() = data!!.sites.toList()
 
-    return results
+  fun getAllHotspots(
+    targetLevel: Int = 120
+  ): List<HotspotInfo> {
+    val hotspotInfos = mutableListOf<HotspotInfo>()
+    getAllSites().forEach { site ->
+      site.hotspots.forEach { hotspot ->
+        if (hotspot.level <= targetLevel) {
+          hotspotInfos += HotspotInfo(
+            hotspot = hotspot,
+            site = site
+          )
+        }
+      }
+    }
+
+    return hotspotInfos.sortedBy { it.hotspot.level }
+  }
+
+  fun getAllArtefacts(
+    targetLevel: Int = 120
+  ): List<ArtefactInfo> {
+    val artefacts = mutableListOf<ArtefactInfo>()
+    getAllHotspots(targetLevel).forEach { hotspotInfo ->
+      if (hotspotInfo.hotspot.level <= targetLevel) {
+        hotspotInfo.hotspot.artefacts.forEach { artefact ->
+          artefacts += ArtefactInfo(
+            artefact = artefact,
+            hotspotInfo = hotspotInfo
+          )
+        }
+      }
+    }
+
+    return artefacts.sortedBy { it.artefact.level }
+  }
+
+  fun getAllMaterials(
+    targetLevel: Int = 120
+  ): List<MaterialInfo> {
+    val materials = mutableListOf<MaterialInfo>()
+    getAllHotspots(targetLevel).forEach { hotspotInfo ->
+      if (hotspotInfo.hotspot.level <= targetLevel) {
+        hotspotInfo.hotspot.materials.forEach { material ->
+          materials += MaterialInfo(
+            material = material,
+            hotspotInfo = hotspotInfo
+          )
+        }
+      }
+    }
+
+    return materials
   }
 }
