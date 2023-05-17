c70bda9f00128617165715a9829ca6a9e58c0113
LucasAlfare <god-lucas@hotmail.com>
2023-05-17 16:01:53 -0300

    included non excavation materials names

--- src/main/kotlin/com/lucasalfare/flarcheology/ArcheologyInfoProvider.kt
+++ src/main/kotlin/com/lucasalfare/flarcheology/ArcheologyInfoProvider.kt
@@ -3,7 +3,12 @@ package com.lucasalfare.flarcheology
 data class HotspotInfo(
   val hotspot: Hotspot,
   val site: Site
-)
+) {
+
+  fun containsMaterial(materialName: String) = hotspot.materials.any { it.name == materialName }
+
+  fun containsArtefact(artefactName: String) = hotspot.artefacts.any { it.name == artefactName }
+}
 
 data class ArtefactInfo(
   val artefact: Artefact,
@@ -18,70 +23,73 @@ data class MaterialInfo(
 private var data: ArcheologyDefinition? = null
 private const val DEFAULT_JSON_DATA_LOCATION = "src/main/resources/archeology_data.json"
 
-// TODO: CREATE a function to find ALL spots until target level
-class ArcheologyInfoProvider(
-  jsonPathname: String = DEFAULT_JSON_DATA_LOCATION
-) {
+val notExcavationMaterials = arrayOf(
+  "Dragonstone",
+  "Molten glass",
+  "Death rune",
+  "Ruby",
+  "White candle",
+  "Bronze bar",
+  "Silver bar",
+  "Sapphire",
+  "Clockwork",
+  "Diamond",
+  "Phoenix feather",
+  "Rope",
+  "Black mushroom ink",
+  "Weapon poison (3)",
+  "Emerald"
+)
+
+class ArcheologyInfoProvider(jsonPathname: String = DEFAULT_JSON_DATA_LOCATION) {
 
   init {
-    if (data == null) {
-      data = getArcheologyJsonDefinitions(jsonPathname)
-    }
+    if (data == null) data = getArcheologyJsonDefinitions(jsonPathname)
   }
 
   fun getAllSites() = data!!.sites.toList()
 
-  fun getAllHotspots(
-    targetLevel: Int = 120
-  ): List<HotspotInfo> {
+  fun getAllHotspots(): List<HotspotInfo> {
     val hotspotInfos = mutableListOf<HotspotInfo>()
     getAllSites().forEach { site ->
       site.hotspots.forEach { hotspot ->
-        if (hotspot.level <= targetLevel) {
-          hotspotInfos += HotspotInfo(
-            hotspot = hotspot,
-            site = site
-          )
-        }
+        hotspotInfos += HotspotInfo(
+          hotspot = hotspot,
+          site = site
+        )
       }
     }
 
-    return hotspotInfos.sortedBy { it.hotspot.level }
+    return hotspotInfos
   }
 
-  fun getAllArtefacts(
-    targetLevel: Int = 120
-  ): List<ArtefactInfo> {
+  fun getAllArtefacts(): List<ArtefactInfo> {
     val artefacts = mutableListOf<ArtefactInfo>()
-    getAllHotspots(targetLevel).forEach { hotspotInfo ->
-      if (hotspotInfo.hotspot.level <= targetLevel) {
-        hotspotInfo.hotspot.artefacts.forEach { artefact ->
-          artefacts += ArtefactInfo(
-            artefact = artefact,
-            hotspotInfo = hotspotInfo
-          )
-        }
+    getAllHotspots().forEach { hotspotInfo ->
+      hotspotInfo.hotspot.artefacts.forEach { artefact ->
+        artefacts += ArtefactInfo(
+          artefact = artefact,
+          hotspotInfo = hotspotInfo
+        )
       }
     }
 
-    return artefacts.sortedBy { it.artefact.level }
+    return artefacts
   }
 
-  fun getAllMaterials(
-    targetLevel: Int = 120
-  ): List<MaterialInfo> {
+  fun getAllMaterials(): List<MaterialInfo> {
     val materials = mutableListOf<MaterialInfo>()
-    getAllHotspots(targetLevel).forEach { hotspotInfo ->
-      if (hotspotInfo.hotspot.level <= targetLevel) {
-        hotspotInfo.hotspot.materials.forEach { material ->
-          materials += MaterialInfo(
-            material = material,
-            hotspotInfo = hotspotInfo
-          )
-        }
+    getAllHotspots().forEach { hotspotInfo ->
+      hotspotInfo.hotspot.materials.forEach { material ->
+        materials += MaterialInfo(
+          material = material,
+          hotspotInfo = hotspotInfo
+        )
       }
     }
 
     return materials
   }
+
+  fun containsMaterial(materialName: String) = getAllMaterials().any { it.material.name == materialName }
 }
